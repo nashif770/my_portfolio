99762f71853d39bea47360a228c2c8cade75213d
Ahmed Nashif <a.nashif7@gmail.com>
2024-02-22 14:16:22 +0600

    projects Section

--- src/app/components/ProjectCard.jsx
+++ src/app/components/ProjectCard.jsx
@@ -1,23 +1,32 @@
 import React from "react";
-import { CodeBracketIcon, CodeBracketSquareIcon } from "@heroicons/react/24/solid";
+import { EyeIcon, CodeBracketSquareIcon } from "@heroicons/react/24/solid";
 import Link from "next/link";
 
-const ProjectCard = ({ imgUrl, title, description }) => {
+const ProjectCard = ({ imgUrl, title, description, gitUrl, previewUrl }) => {
   return (
     <div>
       <div
         className="h-52 md:h-72 rounded-t-xl relative group"
         style={{ background: `url(${imgUrl})`, backgroundSize: "cover" }}
       >
-        <div className="overlay absolute top-0 left-0 w-full h-full bg-[#181818] bg-opacity-0 hidden group-hover:flex group-hover:bg-opacity-80 transition-all duration-500">
-          <Link href={"/"}>
-            <CodeBracketSquareIcon  className="h-10 w-10 text-[#ADB7BE] m-2 cursor-pointer"></CodeBracketSquareIcon>
+        <div className="overlay items-center justify-center absolute top-0 left-0 w-full h-full bg-[#181818] bg-opacity-0 hidden group-hover:flex group-hover:bg-opacity-80 transition-all duration-500">
+          <Link
+            href={gitUrl}
+            className="h-14 mr-2 w-14 border-2 realative rounded-full border-[#ADB7BE] hover:border-white group/Link "
+          >
+            <CodeBracketSquareIcon className="h-10 w-10 text-[#ADB7BE] relative top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 cursor-pointer group-hover/Link:text-white "></CodeBracketSquareIcon>
+          </Link>
+          <Link
+            href={previewUrl}
+            className="h-14 w-14 border-2 realative rounded-full border-[#ADB7BE] hover:border-white group/Link"
+          >
+            <EyeIcon className="h-10 w-10 text-[#ADB7BE] relative top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 cursor-pointer group-hover/Link:text-white "></EyeIcon>
           </Link>
         </div>
       </div>
-      <div className="text-white rounded-b-xl bg-[#181818] py-6 px-4">
-        <h5 className="font-xl font-semibold">{title}</h5>
-        <p className="text-[#ADB7BE]">{description}</p>
+      <div className="text-white rounded-b-xl mt-2 bg-[#181818] py-6 px-4">
+        <h5 className="text-xl font-semibold mb-2">{title}</h5>
+        <p className="text-[#ADB7BE] ">{description}</p>
       </div>
     </div>
   );
--- src/app/components/ProjectSection.jsx
+++ src/app/components/ProjectSection.jsx
@@ -1,50 +1,97 @@
-import React from 'react'
-import ProjectCard from './ProjectCard'
+"use client";
+import React, { useState } from "react";
+import ProjectCard from "./ProjectCard";
+import ProjectTag from "./ProjectTag";
 
 const projectsData = [
-    {
-        id: 1,
-        title: "React Portfolio Website",
-        description: "Project 1 Description",
-        image: "https://i.ibb.co/nRDfBFQ/Banner2.jpg",
-        tag: ["all", "web"]
-
-    },
-    {
-        id: 2,
-        title: "React Portfolio Website",
-        description: "Project 1 Description",
-        image: "https://i.ibb.co/nRDfBFQ/Banner2.jpg",
-        tag: ["all", "web"]
-
-    },
-    {
-        id: 3,
-        title: "React Portfolio Website",
-        description: "Project 1 Description",
-        image: "https://i.ibb.co/nRDfBFQ/Banner2.jpg",
-        tag: ["all", "web"]
-
-    },
-    {
-        id: 4,
-        title: "React Portfolio Website",
-        description: "Project 1 Description",
-        image: "https://i.ibb.co/nRDfBFQ/Banner2.jpg",
-        tag: ["all", "web"]
-
-    },
-]
+  {
+    id: 1,
+    title: "React Portfolio Website",
+    description: "Project 1 Description",
+    image: "https://i.ibb.co/nRDfBFQ/Banner2.jpg",
+    tag: ["All", "Web"],
+    gitUrl: "/",
+    previewUrl: "/",
+  },
+  {
+    id: 2,
+    title: "React Portfolio Website",
+    description: "Project 1 Description",
+    image: "https://i.ibb.co/nRDfBFQ/Banner2.jpg",
+    tag: ["All", "Web"],
+    gitUrl: "/",
+    previewUrl: "/",
+  },
+  {
+    id: 3,
+    title: "React Portfolio Website",
+    description: "Project 1 Description",
+    image: "https://i.ibb.co/nRDfBFQ/Banner2.jpg",
+    tag: ["All", "Web"],
+    gitUrl: "/",
+    previewUrl: "/",
+  },
+  {
+    id: 4,
+    title: "React Portfolio Website",
+    description: "Project 1 Description",
+    image: "https://i.ibb.co/nRDfBFQ/Banner2.jpg",
+    tag: ["All", "Mobile"],
+    gitUrl: "/",
+    previewUrl: "/",
+  },
+];
 
 const ProjectSection = () => {
+  const [tag, setTag] = useState("All");
+
+  const handleTagChange = (newTag) => {
+    setTag(newTag);
+  };
+
+  const filteredProjects = projectsData.filter((project) =>
+    project.tag.includes(tag)
+  );
+
+  console.log("tag", tag);
+  console.log("projects", filteredProjects);
+
   return (
     <>
-    <h2>Project Section</h2>
-    <div>
-        {projectsData.map((project)=> <ProjectCard key={project.id} title={project.title} description={project.description} imgUrl={project.image}></ProjectCard>)}
-    </div>
+      <h2 className="text-center text-4xl font-bold text-white mt-4 mb-8 md:mb-12">
+        Project Section
+      </h2>
+      <div className="text-white flex flex-row justify-center items-center gap-2 py-6">
+        <ProjectTag
+          name={"All"}
+          onClick={handleTagChange}
+          isSelected={tag === "All"}
+        />
+        <ProjectTag
+          name={"Web"}
+          onClick={handleTagChange}
+          isSelected={tag === "Web"}
+        />
+        <ProjectTag
+          name={"Mobile"}
+          onClick={handleTagChange}
+          isSelected={tag === "Mobile"}
+        />
+      </div>
+      <div className="grid md:grid-cols-3 gap-8 md:gap-12">
+        {filteredProjects.map((project) => (
+          <ProjectCard
+            key={project.id}
+            title={project.title}
+            description={project.description}
+            imgUrl={project.image}
+            gitUrl={project.gitUrl}
+            previewUrl={project.previewUrl}
+          ></ProjectCard>
+        ))}
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default ProjectSection
+export default ProjectSection;
